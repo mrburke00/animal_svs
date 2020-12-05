72bede75d09c58400d0848a79581cf1c4b43bb25
Zach <zmcgrath96@gmail.com>
2020-12-04 17:06:37 -0700

    new attempt at logging

--- src/logger.py
+++ src/logger.py
@@ -5,13 +5,6 @@ import os
 
 default_log_file = os.path.join(os.path.expanduser('~'), 'logs.log')
 
-f = subprocess.Popen(['tail', '-F', default_log_file],\
-        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        
-p = select.poll()
-p.register(f.stdout)
-
-while True:
-    if p.poll(1):
-        print f.stdout.readline()
+with open(default_log_file, 'r') as f:
     time.sleep(1)
+    print(f.readline())
