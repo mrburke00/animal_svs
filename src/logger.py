9df13a08c27cc657733ad85365b5175108c495aa
Zach <zmcgrath96@gmail.com>
2020-12-04 17:20:07 -0700

    another attempt

--- src/logger.py
+++ src/logger.py
@@ -1,10 +1,18 @@
 import time
-import subprocess
-import select
-import os
 
 default_log_file = os.path.join(os.path.expanduser('~'), 'logs.log')
 
-with open(default_log_file, 'r') as f:
-    time.sleep(1)
-    print(f.readline())
+def follow(thefile):
+    thefile.seek(0,2)
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+f = open(default_log_file)
+lines = follow(f)
+
+for i in lines:
+    print (i)
