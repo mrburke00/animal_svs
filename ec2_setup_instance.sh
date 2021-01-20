097c46637f28f9b41321a0d9f32c882a386f1f01
Murad <mchowdh200@gmail.com>
2021-01-20 13:21:36 -0700

    Added ec2 setup script.

---
+++ ec2_setup_instance.sh
@@ -0,0 +1,46 @@
+#!/bin/env bash
+
+set exuo pipefail
+
+# some common dependencies ------------------------------------------------------------------------
+sudo apt-get update
+sudo apt-get install -y \
+    build-essential curl git libcurl4-openssl-dev wget \
+    software-properties-common automake libtool pkg-config libssl-dev \
+    ncurses-dev awscli python-pip libbz2-dev liblzma-dev unzip
+
+# mount storage -----------------------------------------------------------------------------------
+export TMPDIR=/mnt/local/temp
+sudo mkfs -t ext4 /dev/nvme0n1 
+
+sudo mkdir /mnt/local
+sudo mount /dev/nvme0n1 /mnt/local
+sudo chown ubuntu /mnt/local
+
+mkdir /mnt/local/data
+mkdir /mnt/local/temp
+echo "export TMPDIR=/mnt/local/temp" >> ~/.profile
+
+# tmux/neovim setup -------------------------------------------------------------------------------
+echo "source-file ~/.tmux.d/.tmux.conf" > ~/.tmux.conf
+git clone https://github.com/mchowdh200/.tmux.d.git ~/.tmux.d
+
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo apt-get update -y
+sudo apt-get install -y neovim
+git clone https://github.com/mchowdh200/.vim.git ~/.vim
+mkdir ~/.config
+mkdir ~/.config/nvim
+printf "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath=&runtimepath\nsource ~/.vim/vimrc" > ~/.config/nvim/init.vim
+pip install jedi neovim
+echo "alias vim=nvim" >> ~/.profile
+echo "export EDITOR=nvim" >> ~/.profile
+
+# setup path --------------------------------------------------------------------------------------
+mkdir /mnt/local/bin
+echo "PATH=$PATH:/mnt/local/bin" >> ~/.profile
+
+# install gargs -----------------------------------------------------------------------------------
+wget https://github.com/brentp/gargs/releases/download/v0.3.9/gargs_linux -O /mnt/local/bin/gargs
+chmod +x /mnt/local/bin/gargs
+
