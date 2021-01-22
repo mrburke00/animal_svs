eada1cd9e5390dfde2f24c78ca3ad598de0d5c6c
Ubuntu <ubuntu@ip-172-31-66-51.ec2.internal>
2021-01-22 02:01:45 +0000

    fix parallel_simulator bug.

--- parallel_simulator.sh
+++ parallel_simulator.sh
@@ -7,10 +7,10 @@ function simreads() {
     local n_reads=$2
     local out_name=$3
     local part=$4
-    local seed=$RANDOM
+    local seed=$(od -An -td4 -N4 < /dev/urandom) # random 32 bit integer
 
-    perl simulator.pl -random-seed $seed \
-                      -output-format "${out_name}_${part}_#.fq"
+    perl simulator.pl $fasta -random-seed $seed \
+                      -output-format ${out_name}_${part}#.fq \
                       -num-frags $n_reads \
                       -frag-dist-params "456,70" \
                       -read-length 100
@@ -21,6 +21,6 @@ processes=$1
 fasta=$2
 out_name=$3
 
-n_reads=$(python -c '135000000//36')
+n_reads=$(python3 -c 'print(135000000//36, end="")')
 
-seq 1 36 | gargs "simreads $fasta $n_reads $out_name {}"
+seq 1 36 | gargs -p $processes "simreads $fasta $n_reads $out_name {}"
