05178bae90fa34c9109f5818fe64008eeb25d7fc
Murad <mchowdh200@gmail.com>
2021-03-09 09:49:34 -0700

    changes to ec2 instance setup.

--- ec2_setup_instance.sh
+++ ec2_setup_instance.sh
@@ -2,14 +2,15 @@
 
 set exuo pipefail
 
-# some common dependencies ------------------------------------------------------------------------
+## some common dependencies
 sudo apt-get update
 sudo apt-get install -y \
     build-essential curl git libcurl4-openssl-dev wget \
     software-properties-common automake libtool pkg-config libssl-dev \
     ncurses-dev awscli python-pip libbz2-dev liblzma-dev unzip
 
-# mount storage -----------------------------------------------------------------------------------
+## mount storage
+## TODO change to raid 0
 export TMPDIR=/mnt/local/temp
 sudo mkfs -t ext4 /dev/nvme0n1 
 
@@ -19,10 +20,10 @@ sudo chown ubuntu /mnt/local
 
 mkdir /mnt/local/data
 mkdir /mnt/local/temp
-echo "export TMPDIR=/mnt/local/temp" >> ~/.profile
+echo 'export TMPDIR=/mnt/local/temp' >> ~/.profile
 
-# tmux/neovim setup -------------------------------------------------------------------------------
-echo "source-file ~/.tmux.d/.tmux.conf" > ~/.tmux.conf
+## tmux/neovim setup
+echo 'source-file ~/.tmux.d/.tmux.conf' > ~/.tmux.conf
 git clone https://github.com/mchowdh200/.tmux.d.git ~/.tmux.d
 
 sudo add-apt-repository ppa:neovim-ppa/stable -y
@@ -31,25 +32,25 @@ sudo apt-get install -y neovim
 git clone https://github.com/mchowdh200/.vim.git ~/.vim
 mkdir ~/.config
 mkdir ~/.config/nvim
-printf "set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath=&runtimepath\nsource ~/.vim/vimrc" > ~/.config/nvim/init.vim
+printf 'set runtimepath^=~/.vim runtimepath+=~/.vim/after\nlet &packpath=&runtimepath\nsource ~/.vim/vimrc' > ~/.config/nvim/init.vim
 pip install jedi neovim
-echo "alias vim=nvim" >> ~/.profile
-echo "export EDITOR=nvim" >> ~/.profile
+echo 'alias vim=nvim' >> ~/.profile
+echo 'export EDITOR=nvim' >> ~/.profile
 
-# setup path --------------------------------------------------------------------------------------
+## setup path
 mkdir /mnt/local/bin
-echo "PATH=$PATH:/mnt/local/bin" >> ~/.profile
+echo 'PATH=$PATH:/mnt/local/bin' >> ~/.profile
 
-# install gargs -----------------------------------------------------------------------------------
+## install gargs
 wget https://github.com/brentp/gargs/releases/download/v0.3.9/gargs_linux -O /mnt/local/bin/gargs
 chmod +x /mnt/local/bin/gargs
 
-# conda setup -------------------------------------------------------------------------------------
+## conda setup
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 eval "$($HOME/miniconda/bin/conda shell.bash hook)"
 conda init
 conda config --add channels bioconda
 
-# setup snakemake ----------------------------------------------------------------------------------
-conda create -c bioconda -y -n snakemake snakemake boto3 boto
+## setup snakemake
+mamba create -c bioconda -y -n snakemake snakemake boto3 boto
