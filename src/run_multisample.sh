9f4f6a05410e1e5dbc5021e8118f304b49ba8b5c
Murad <mchowdh200@gmail.com>
2021-03-09 13:58:07 -0700

    forgot line contiunation.

--- src/run_multisample.sh
+++ src/run_multisample.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
 snakemake -s multisample.smk \
-          -j $(grep -c ^processor /proc/cpuinfo)
+          -j $(grep -c ^processor /proc/cpuinfo) \
           --resources disk_mb=100000
