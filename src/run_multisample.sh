5d3b7ef35c6949fcddc3f5b8cf55a5a7f0c98673
Murad <mchowdh200@gmail.com>
2021-03-09 14:03:20 -0700

    need to use conda environments.

--- src/run_multisample.sh
+++ src/run_multisample.sh
@@ -2,4 +2,5 @@
 
 snakemake -s multisample.smk \
           -j $(grep -c ^processor /proc/cpuinfo) \
-          --resources disk_mb=100000
+          --resources disk_mb=100000 \
+          --use-conda
