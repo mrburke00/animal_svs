c787b19071b485fb3c093fa081bb637e23684c96
Murad <mchowdh200@gmail.com>
2021-03-09 14:06:35 -0700

    test smaller disk space.

--- src/run_multisample.sh
+++ src/run_multisample.sh
@@ -2,5 +2,5 @@
 
 snakemake -s multisample.smk \
           -j $(grep -c ^processor /proc/cpuinfo) \
-          --resources disk_mb=100000 \
+          --resources disk_mb=50000 \
           --use-conda
