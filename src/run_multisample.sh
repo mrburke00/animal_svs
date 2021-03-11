3ee855c0b39323a93f75a44e0b1f1078c318c41c
Murad <mchowdh200@gmail.com>
2021-03-11 14:39:13 -0700

    modify run script to do two snakefiles.

--- src/run_multisample.sh
+++ src/run_multisample.sh
@@ -8,9 +8,8 @@ snakemake -s multisample.smk \
           --conda-prefix /mnt/local/snakemake-conda \
           --scheduler greedy
 
-snakemake -s multisample.smk \
+snakemake -s genotype.smk \
           -j $(grep -c ^processor /proc/cpuinfo) \
-          --until AllGenotype \
           --resources disk_mb=1200000 \
           --use-conda --conda-frontend mamba \
           --conda-prefix /mnt/local/snakemake-conda \
