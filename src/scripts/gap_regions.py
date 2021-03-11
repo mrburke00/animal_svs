120ea10acda47bd7d6be23cd429b7530af9f2f34
Murad <mchowdh200@gmail.com>
2021-03-10 22:47:04 -0700

    removed commented out code.

--- src/scripts/gap_regions.py
+++ src/scripts/gap_regions.py
@@ -1,41 +1,10 @@
 """
 Taken from https://www.biostars.org/p/133742/
+Parallelized by record.
 """
-
 import sys
 from Bio import SeqIO
 from joblib import Parallel, delayed
-
-fasta = sys.argv[1]
-processes = int(sys.argv[2])
-
-# with open(fasta, mode="r") as fasta_handle:
-    # for record in SeqIO.parse(fasta_handle, "fasta"):
-    #     start_pos = 0
-    #     counter = 0
-    #     gap = False
-    #     gap_length = 0
-    #     for char in record.seq:
-    #         if char == "N":
-    #             if gap_length == 0:
-    #                 start_pos = counter
-    #                 gap_length = 1
-    #                 gap = True
-    #             else:
-    #                 gap_length += 1
-    #         else:
-    #             if gap:
-    #                 print(
-    #                     record.id
-    #                     + "\t"
-    #                     + str(start_pos)
-    #                     + "\t"
-    #                     + str(start_pos + gap_length)
-    #                 )
-    #                 gap_length = 0
-    #                 gap = False
-    #         counter += 1
-
 
 def get_gap_regions(record):
     regions = []
@@ -61,9 +30,10 @@ def get_gap_regions(record):
     return regions
 
 if __name__ == "__main__":
+    fasta = sys.argv[1]
+    processes = int(sys.argv[2])
     with open(fasta, mode="r") as fasta_handle:
-        gap_regions = Parallel(n_jobs=processes)(
-            delayed(get_gap_regions)(record)
+        gap_regions = Parallel(n_jobs=processes)(delayed(get_gap_regions)(record)
             for record in SeqIO.parse(fasta_handle, "fasta"))
         for regions in gap_regions:
             for r in regions:
